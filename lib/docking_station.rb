51d341d53cd8035eeb983849b9a5768ce5d76be7
Josh Harris <josh.harris212@yahoo.co.uk>
2018-10-02 14:37:23 +0000

    step 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,15 +2,15 @@ require_relative 'bike'
 
 class DockingStation
   def release_bike
-      Bike.new
+    fail 'No bikes available' unless @bike
+      @bike
   end
-  def bike   
-  end
+  
   def dock(bike)
+    fail 'Docking station full' if @bike
     @bike = bike
   end
   attr_reader :bike
 end
-
 
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,26 @@
-require "docking_station"
-describe DockingStation do 
-    it 'responds to release_bike' do
-        expect(subject).to respond_to :release_bike
-    end
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
-  it { is_expected.to respond_to(:dock).with(1).argument }
-  it 'returns docked bikes' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.dock(bike)).to eq bike
-  end
+require 'docking_station'
+describe DockingStation do
+   it 'responds to release_bike' do
+       expect(subject).to respond_to :release_bike
+   end
+ describe '#release_bike' do
+   it 'releases a bike' do
+     bike = Bike.new
+     subject.dock(bike)
+     expect(subject.release_bike).to eq bike
+   end
+ end
+ it { is_expected.to respond_to(:dock).with(1).argument }
+
+ describe '#release_bike' do
+   it 'raises an error when there are no bikes available' do
+     expect { subject.release_bike }.to raise_error 'No bikes available'
+   end
+ end
+ describe '#dock' do
+   it 'raises an error when full' do
+     subject.dock(Bike.new)
+     expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+   end
+ end
 end
