da2520a48842c095c3713e61f9e909e1a72c8351
Josh Harris <josh.harris212@yahoo.co.uk>
2018-10-03 12:01:14 +0000

    finished step 17

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,20 +1,36 @@
 require_relative 'bike'
 
 class DockingStation
-  def initialize 
+  
+  DEAFULT_CAPACITY = 20
+  attr_accessor :capacity
+  attr_reader :capacity
+  
+  def initialize(capacity=DEAFULT_CAPACITY)
+    @capacity = capacity
     @bikes = []
   end
   
   def release_bike
-    fail 'No bikes available' if  @bikes.empty?
+    fail 'No bikes available' if empty?
       @bikes.pop
   end
   
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full'  if full?
     @bikes << bike
   end
-  #attr_reader :bike
+  
+  private 
+  attr_reader :bikes
+  
+  def full?
+    @bikes.count >= DEAFULT_CAPACITY
+  end
+  
+  def empty?
+    @bikes.empty?
+  end
 end
 
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,14 @@
 require 'docking_station'
 describe DockingStation do
+    
+   it "has a deafuly capacity" do
+       expect(subject.capacity).to eq DockingStation::DEAFULT_CAPACITY
+   end
+   
    it 'responds to release_bike' do
        expect(subject).to respond_to :release_bike
    end
+   
  describe '#release_bike' do
    it 'releases a bike' do
      bike = Bike.new
@@ -10,6 +16,7 @@ describe DockingStation do
      expect(subject.release_bike).to eq bike
    end
  end
+ 
  it { is_expected.to respond_to(:dock).with(1).argument }
 
  describe '#release_bike' do
@@ -17,10 +24,25 @@ describe DockingStation do
      expect { subject.release_bike }.to raise_error 'No bikes available'
    end
  end
+ 
 describe '#dock' do
     it 'raises an error when full' do
-      20.times { subject.dock Bike.new }
-      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+     subject.capacity.times { subject.dock Bike.new }
+     expect { subject.dock Bike.new }.to raise_error "Docking station full"
     end
   end
-end
+  
+  describe 'initialization' do 
+      
+     subject { DockingStation.new }
+     let(:bike) { Bike.new }
+     it 'defaults capcity' do
+         described_class::DEAFULT_CAPACITY.times do
+             subject.dock(bike)
+      end
+      expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+   end
+  end
+ end
+ 
+
